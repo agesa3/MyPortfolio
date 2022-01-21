34223a78e65e4eb10ae4daf8b54fbe6bf69518b5
agesa3 <collinsagesa2000@gmail.com>
2022-01-21 22:26:42 +0300

    navbar done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,55 @@
 import Link from 'next/link';
 import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { AiFillGithub, AiFillLinkedin,AiFillTwitterCircle } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
 import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
 
 const Header = () =>  (
   <div>
-   
+   <Container>
+     <Div1>
+       <Link href="/">
+          <a style={{display:"flex",alignItems:"center",color:"white"}}>
+          <DiCssdeck size="3rem" /><span>Agesa Portfolio</span>
+          </a>
+       </Link>
+     </Div1>
+     <Div2>
+       <li>
+         <Link href="#projects">
+           <NavLink>
+             Projects
+           </NavLink>
+         </Link>
+       </li>
+       <li>
+         <Link href="#tech">
+           <NavLink>
+             Technologies
+           </NavLink>
+         </Link>
+       </li>
+       <li>
+         <Link href="#about">
+           <NavLink>
+             About
+           </NavLink>
+         </Link>
+       </li>
+     </Div2>
+     <Div3>
+       <SocialIcons href="https://github.com/agesa3">
+          <AiFillGithub size="3rem" />
+       </SocialIcons>
+       <SocialIcons href="https://www.linkedin.com/in/agesa-collins-15b15917a/">
+          <AiFillLinkedin size="3rem" />
+       </SocialIcons>
+       <SocialIcons href="https://twitter.com/AgesaCollins3">
+          <AiFillTwitterCircle size="3rem" />
+       </SocialIcons>
+     </Div3>
+   </Container>
   </div>
 );
 
