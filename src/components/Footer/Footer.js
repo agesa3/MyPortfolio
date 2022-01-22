d426b963c4b9bb0280578bd6857ff490a44ebda2
agesa3 <collinsagesa2000@gmail.com>
2022-01-22 12:01:22 +0300

    done working on the footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,49 @@
-import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import React from "react";
+import { AiFillGithub, AiFillTwitterCircle, AiFillLinkedin } from "react-icons/ai";
 
-import { SocialIcons } from '../Header/HeaderStyles';
-import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+import { SocialIcons } from "../Header/HeaderStyles";
+import {
+  CompanyContainer,
+  FooterWrapper,
+  LinkColumn,
+  LinkItem,
+  LinkList,
+  LinkTitle,
+  Slogan,
+  SocialContainer,
+  SocialIconsContainer,
+} from "./FooterStyles";
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkItem>Call</LinkItem>
+          <LinkItem href="tel:+254708593702">+254 7085 937 02</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkItem>Email</LinkItem>
+          <LinkItem href="mailto:agesacollins4299@gmail.com">agesacollins4299@gmail.com</LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Innovating one project at a time</Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+        <SocialIcons href="https://github.com/agesa3">
+          <AiFillGithub size="3rem" />
+       </SocialIcons>
+       <SocialIcons href="https://www.linkedin.com/in/agesa-collins-15b15917a/">
+          <AiFillLinkedin size="3rem" />
+       </SocialIcons>
+       <SocialIcons href="https://twitter.com/AgesaCollins3">
+          <AiFillTwitterCircle size="3rem" />
+       </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
